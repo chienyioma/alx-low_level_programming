6b92ba657209746cbdc2dbe1b1e22bbb95e7c82a
chienyioma <chienyioma19@gmail.com>
2022-06-30 19:32:43 -0700

    Code that prints

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-/*
+/**
 *main-Entry point
-* Description: 'This prints out a sentence'
+*Description: 'This prints out a sentence'
 *Return:Always 0 (success)
 */
 int main(void)
- {
-  puts("\"Programming is like building a multilingual puzzle");
-  return(0);
- }
+{
+puts("\"Programming is like building a multilingual puzzle");
+return(0);
+}
