f8b6cb6c9975423f613a714c825453bcf0ff69b9
chienyioma <chienyioma19@gmail.com>
2022-07-02 11:42:14 -0700

    Exclude q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - Prints alphabets in lower case
+* Description: 'This will exclude alphabets q and e'
+* Return: Always 0.
+*/
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{	
+		if(ch != 'e' && ch != 'q')
+			putchar(ch);
+	}		
+	putchar('\n');
+	return (0);
+}
