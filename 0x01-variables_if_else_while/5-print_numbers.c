689c73a20ebf1691d5c351d4f2881539f5d4e797
chienyioma <chienyioma19@gmail.com>
2022-07-02 13:42:56 -0700

    Single digits from 0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - Prints alphabets in lower case
+* Description: 'Print all single digit number from 0'
+* Return: Always 0.
+*/
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+		printf("%d", a);
+	printf("\n");
+	return (0);
+}
