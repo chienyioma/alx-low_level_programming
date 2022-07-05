61956950203bc2ee151a3fcc4fc658d8d10e27a3
chienyioma <chienyioma19@gmail.com>
2022-07-05 15:41:36 -0700

    Print lowercase alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* main - Entry point
+* Description: 'Prints alphabet in lowercase'
+* Return: Always 0.
+*/
+void print_alphabet(void);
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+	putchar('\n');
+	return (0);
+}
