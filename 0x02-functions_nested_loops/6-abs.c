9e12c268598e98d2c0d6de085718ff5435188e19
chienyioma <chienyioma19@gmail.com>
2022-07-05 19:55:34 -0700

    Check for sign of a number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - Prints absolute value
+ * @int: The int to print
+ * Return: 0 Success
+ */
+int _abs(int)
+{
+	if (int > 0)
+		return (int);
+	else
+		return (-int);
+}
