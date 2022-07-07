186105b16d008a442367f96fe77c547767fdc822
chienyioma <chienyioma19@gmail.com>
2022-07-07 05:58:58 -0700

    Print digits 0 to 9 excluding 2 & 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,14 +6,13 @@
  */
 void print_most_numbers(void)
 {
-
 	char a;
 
-		for (a = '0'; a <= '9'; a++)
-		{
-			if (a != '2' && a != '4')
-				_putchar('a');
-		}
+	for (a = '0'; a <= '9'; a++)
+	{
+		if (a != '2' && a != '4')
+			_putchar('a');
+	}
 
-		_putchar('\n');
+	_putchar('\n');
 }
