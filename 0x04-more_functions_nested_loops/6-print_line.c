93e1b8b4d7e6b092a5cbf42c6b3132077066d421
chienyioma <chienyioma19@gmail.com>
2022-07-07 08:27:35 -0700

    Draw a line using -

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_line - Draws a straight line using the character _.
+ * print_line - Draws a straight line using the character _
  * @n: The number of _ to be printed
  */
 void print_line(int n)
@@ -14,6 +14,8 @@ void print_line(int n)
 			_putchar('_');
 
 	}
+	else
+		_putchar('///n');
 	_putchar('\n');
 
 }
