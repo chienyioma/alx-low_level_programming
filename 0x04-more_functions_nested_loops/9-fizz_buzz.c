28aeb8046d59f4e383ebf03d4ba47624a5a5e6b5
chienyioma <chienyioma19@gmail.com>
2022-07-07 15:55:54 -0700

    Print a digits 1 to 100 skipping multiples of 3 and 5

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,14 +13,16 @@ int main(void)
 
 	for (n = 1; n <= 100; n++)
 	{
-		if (n % 3 == 0)
-			printf("Fizz");
+		if (n == 1)
+			printf("%d", n)
+		else if (n % 3 == 0)
+			printf(" Fizz");
 		else if (n % 5 == 0)
-			printf("Buzz");
+			printf(" Buzz");
 		else if (n % 3 == 0 && n % 5 == 0)
-			printf("FizzBuzz");
+			printf(" FizzBuzz");
 		else
-			printf("%d", n);
+			printf(" %d", n);
 	}
 
 	printf("\n");
