6a0836310809181375a4f03e7d7984518b6d60fd
chienyioma <chienyioma19@gmail.com>
2022-07-12 14:17:08 -0700

    Set pointer parameter and update value

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer to a parameter and updates its value.
+ * @n: interger to be inputted.
+ * Return: no return.
+ */
+void reset_to_98(int *n)
+{
+
+	*n = 98;
+
+}
